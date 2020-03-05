286c6d77f7c2e82f0fc1e090f36081a558a37b0b
Robson Peacock <robson_peacock20@outlook.com>
2020-03-05 21:45:54 +0000

    Updated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
+3.times do |topic|
+  Topic.create!(title: "Topic #{topic}")
+end
+
+puts "3 Topics created"
+
 10.times do |blog|
   Blog.create!(
     title: "My Blog Post #{blog}",
-    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque viverra erat id lacus auctor, dignissim aliquam ipsum porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer pulvinar rhoncus lacus et rhoncus. Phasellus faucibus hendrerit ornare. Fusce non justo eros. Aliquam libero leo, iaculis ac lacinia eu, aliquam pulvinar est. In vitae interdum felis, quis pharetra tortor. Nam sit amet enim augue. Vivamus quis tincidunt nulla. Phasellus feugiat id mi sit amet faucibus. Quisque viverra lorem at ipsum posuere pharetra. Pellentesque laoreet, purus finibus volutpat imperdiet, nisl velit commodo urna, sit amet vehicula nunc nisi vitae purus. Morbi sit amet turpis vel lacus tempus suscipit."
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque viverra erat id lacus auctor, dignissim aliquam ipsum porta. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer pulvinar rhoncus lacus et rhoncus. Phasellus faucibus hendrerit ornare. Fusce non justo eros. Aliquam libero leo, iaculis ac lacinia eu, aliquam pulvinar est. In vitae interdum felis, quis pharetra tortor. Nam sit amet enim augue. Vivamus quis tincidunt nulla. Phasellus feugiat id mi sit amet faucibus. Quisque viverra lorem at ipsum posuere pharetra. Pellentesque laoreet, purus finibus volutpat imperdiet, nisl velit commodo urna, sit amet vehicula nunc nisi vitae purus. Morbi sit amet turpis vel lacus tempus suscipit.",
+    topic_id: Topic.last.id
   )
 end
 
@@ -18,10 +25,20 @@ puts "5 Skills were successfully created"
 
 puts "9 Portfolio items were successfully created"
 
-9.times do |portfolio|
+8.times do |portfolio|
   Portfolio.create!(
     title: "Portfolio #{portfolio}",
-    subtitle: "subtitle for portfolio #{portfolio}",
+    subtitle: "Ruby on rails",
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget tempor dolor. Vivamus consequat nulla a tellus suscipit eleifend. Proin tincidunt rutrum volutpat. Fusce at est fringilla, varius nulla id, fermentum augue. Maecenas a lorem mi. Praesent ut dignissim eros, eget hendrerit neque. Aliquam erat volutpat.",
+    main_image: "https://via.placeholder.com/500x400",
+    thumb_image: "https://via.placeholder.com/250x200"
+  )
+end
+
+1.times do |portfolio|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio}",
+    subtitle: "Angular",
     body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus eget tempor dolor. Vivamus consequat nulla a tellus suscipit eleifend. Proin tincidunt rutrum volutpat. Fusce at est fringilla, varius nulla id, fermentum augue. Maecenas a lorem mi. Praesent ut dignissim eros, eget hendrerit neque. Aliquam erat volutpat.",
     main_image: "https://via.placeholder.com/500x400",
     thumb_image: "https://via.placeholder.com/250x200"
