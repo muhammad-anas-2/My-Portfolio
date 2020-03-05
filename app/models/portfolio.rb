06cc314985cf393239f579eb1e0c93128e0f4cb1
Robson Peacock <robson_peacock20@outlook.com>
2020-03-05 23:28:00 +0000

    Implemented image defaults for portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,10 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/500x400"
+    self.thumb_image ||= "https://via.placeholder.com/250x200"
+  end
 end
