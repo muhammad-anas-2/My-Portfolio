f08c8361901d55aa6fc807955d88bbfbe7f15b6b
Robson Peacock <robson_peacock20@outlook.com>
2020-05-08 01:53:12 +0100

    Update file upload forms and refactor placeholder functionality

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,20 +3,12 @@ class Portfolio < ApplicationRecord
   accepts_nested_attributes_for :technologies,
                                 reject_if: lambda { |attr| attr['name'].blank? }
 
-  include Placeholder
-  validates_presence_of :title, :body, :main_image, :thumb_image
+  validates_presence_of :title, :body
 
   mount_uploader :thumb_image, PortfolioUploader
   mount_uploader :main_image, PortfolioUploader
 
   def self.by_position
     order('position ASC')
-  end
-
-  after_initialize :set_defaults
-
-  def set_defaults
-    self.main_image ||= Placeholder.image_genarator(height: '500', width: '400')
-    self.thumb_image ||= Placeholder.image_genarator(height: '250', width: '200')
   end
 end
