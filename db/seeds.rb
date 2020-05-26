e6c2f16b8e4851b9892d0919580411cd79a1726e
Robson Peacock <robson_peacock20@outlook.com>
2020-05-26 20:39:47 +0100

    Update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,22 @@
+User.create!(
+  email: "test@test.com",
+  password: "Password123",
+  password_confirmation: "Password123",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "Password123",
+  password_confirmation: "Password123",
+  name: "Regular User"
+)
+
+puts "1 regular user created"
+
 3.times do |topic|
   Topic.create!(title: "Topic #{topic}")
 end
