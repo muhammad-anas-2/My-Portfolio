6094a00ff8e1e0170a312b088dd9d4bbeb9c8dc3
Robson Peacock <robson_peacock20@outlook.com>
2020-05-27 01:56:42 +0100

    Hide contact and blog pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -32,16 +32,8 @@ module ApplicationHelper
         title: 'About'
       },
       {
-        url: contact_path,
-        title: 'Contact'
-      },
-      {
         url: blogs_path,
         title: 'Blog'
-      },
-      {
-        url: portfolios_path,
-        title: 'Portfolio'
       },
       {
         url: tech_news_path,
